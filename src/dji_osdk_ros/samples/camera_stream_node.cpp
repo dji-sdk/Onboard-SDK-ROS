e6e9efd0cf364686f4b78ca4ff643c72e3fcb078
DJI-Jerry <dev.osdk.jerry@dji.com>
2020-08-17 20:44:23 +0800

    fix:fix camera_stream_node
    Fix camera_stream_node and remove useless code.

--- src/dji_osdk_ros/samples/camera_stream_node.cpp
+++ src/dji_osdk_ros/samples/camera_stream_node.cpp
@@ -28,120 +28,269 @@
 
 //INCLUDE
 #include <ros/ros.h>
-#include <dji_osdk_ros/common_type.h>
+#include <iostream>
+
+#include <dji_osdk_ros/SetupCameraH264.h>
 #include <sensor_msgs/Image.h>
-#include <stdio.h>
-#include <dji_camera_image.hpp>
-#include <dji_osdk_ros/SetupCameraStream.h>
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
 
 #ifdef OPEN_CV_INSTALLED
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #endif
 
+
 //CODE
 using namespace dji_osdk_ros;
 
-void show_rgb(CameraRGBImage img, void *p)
+AVCodecContext*       pCodecCtx;
+AVCodec*              pCodec;
+AVCodecParserContext* pCodecParserCtx;
+SwsContext*           pSwsCtx;
+AVFrame* pFrameYUV;
+AVFrame* pFrameRGB;
+uint8_t* rgbBuf;
+size_t   bufSize;
+
+bool ffmpeg_init()
 {
-    std::string name = std::string(reinterpret_cast<char *>(p));
-    std::cout << "#### Got image from:\t" << name << std::endl;
+    avcodec_register_all();
+    pCodecCtx = avcodec_alloc_context3(NULL);
+    if (!pCodecCtx)
+    {
+        return false;
+    }
+
+    pCodecCtx->thread_count = 4;
+    pCodec = avcodec_find_decoder(AV_CODEC_ID_H264);
+    if (!pCodec || avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
+    {
+        return false;
+    }
+
+    pCodecParserCtx = av_parser_init(AV_CODEC_ID_H264);
+    if (!pCodecParserCtx)
+    {
+        return false;
+    }
+
+    pFrameYUV = av_frame_alloc();
+    if (!pFrameYUV)
+    {
+        return false;
+    }
+
+    pFrameRGB = av_frame_alloc();
+    if (!pFrameRGB)
+    {
+        return false;
+    }
+
+    pSwsCtx = NULL;
+
+    pCodecCtx->flags2 |= AV_CODEC_FLAG2_SHOW_ALL;
+}
+
+void show_rgb(uint8_t *rawData, int height, int width)
+{
 #ifdef OPEN_CV_INSTALLED
-    cv::Mat mat(img.height, img.width, CV_8UC3, img.rawData.data(), img.width*3);
+    cv::Mat mat(height, width, CV_8UC3, rawData, width*3);
     cv::cvtColor(mat, mat, cv::COLOR_RGB2BGR);
-    cv::imshow(name,mat);
+    cv::imshow("camera_stream_node", mat);
     cv::waitKey(1);
 #endif
 }
 
-void fpvCameraStreamCallBack(const sensor_msgs::Image& msg)
+void decodeToDisplay(uint8_t *buf, int bufLen)
 {
-    CameraRGBImage img;
-    img.rawData = msg.data;
-    img.height  = msg.height;
-    img.width   = msg.width;
-    char Name[] = "FPV_CAM";
-    show_rgb(img, &Name);
-    std::cout<<"height is"<<msg.height<<std::endl;
-    std::cout<<"width is"<<msg.width<<std::endl;
+    uint8_t* pData   = buf;
+    int remainingLen = bufLen;
+    int processedLen = 0;
+
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    while (remainingLen > 0)
+    {
+        processedLen = av_parser_parse2(pCodecParserCtx, pCodecCtx,
+                                        &pkt.data, &pkt.size,
+                                        pData, remainingLen,
+                                        AV_NOPTS_VALUE, AV_NOPTS_VALUE, AV_NOPTS_VALUE);
+        remainingLen -= processedLen;
+        pData        += processedLen;
+
+        if (pkt.size > 0)
+        {
+            int gotPicture = 0;
+            avcodec_decode_video2(pCodecCtx, pFrameYUV, &gotPicture, &pkt);
+
+            if (!gotPicture)
+            {
+                //DSTATUS_PRIVATE("Got Frame, but no picture\n");
+                continue;
+            }
+            else
+            {
+                int w = pFrameYUV->width;
+                int h = pFrameYUV->height;
+                //DSTATUS_PRIVATE("Got picture! size=%dx%d\n", w, h);
+
+                if(NULL == pSwsCtx)
+                {
+                    pSwsCtx = sws_getContext(w, h, pCodecCtx->pix_fmt,
+                                             w, h, AV_PIX_FMT_RGB24,
+                                             4, NULL, NULL, NULL);
+                }
+
+                if(NULL == rgbBuf)
+                {
+                    bufSize = avpicture_get_size(AV_PIX_FMT_RGB24, w, h);
+                    rgbBuf = (uint8_t*) av_malloc(bufSize);
+                    avpicture_fill((AVPicture*)pFrameRGB, rgbBuf, AV_PIX_FMT_RGB24, w, h);
+                }
+
+                if(NULL != pSwsCtx && NULL != rgbBuf)
+                {
+                    sws_scale(pSwsCtx,
+                              (uint8_t const *const *) pFrameYUV->data, pFrameYUV->linesize, 0, pFrameYUV->height,
+                              pFrameRGB->data, pFrameRGB->linesize);
+
+                    pFrameRGB->height = h;
+                    pFrameRGB->width = w;
+
+#ifdef OPEN_CV_INSTALLED
+                    cv::Mat mat(pFrameRGB->height, pFrameRGB->width, CV_8UC3, pFrameRGB->data[0], pFrameRGB->width * 3);
+                    cv::cvtColor(mat, mat, cv::COLOR_RGB2BGR);
+                    cv::imshow("camera_stream_node", mat);
+                    cv::waitKey(1);
+#endif
+                }
+            }
+        }
+    }
+    av_free_packet(&pkt);
 }
 
-void mainCameraStreamCallBack(const sensor_msgs::Image& msg)
+void cameraH264CallBack(const sensor_msgs::Image& msg)
 {
-    CameraRGBImage img;
-    img.rawData = msg.data;
-    img.height  = msg.height;
-    img.width   = msg.width;
-    char Name[] = "MAIN_CAM";
-    show_rgb(img, &Name);
-    std::cout<<"height is"<<msg.height<<std::endl;
-    std::cout<<"width is"<<msg.width<<std::endl;
+  printf("msg.data.size() = %lu\n", msg.data.size());
+  decodeToDisplay((uint8_t *)&msg.data[0], msg.data.size());
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "camera_stream_node");
     ros::NodeHandle nh;
-    auto setup_camera_stream_client = nh.serviceClient<dji_osdk_ros::SetupCameraStream>("setup_camera_stream");
-    auto fpv_camera_stream_sub = nh.subscribe("dji_osdk_ros/fpv_camera_images", 10, fpvCameraStreamCallBack);
-    auto main_camera_stream_sub = nh.subscribe("dji_osdk_ros/main_camera_images", 10, mainCameraStreamCallBack);
-    dji_osdk_ros::SetupCameraStream setupCameraStream_;
+    auto setup_camera_h264_client = nh.serviceClient<dji_osdk_ros::SetupCameraH264>("setup_camera_h264");
+    auto fpv_camera_h264_sub = nh.subscribe("dji_osdk_ros/camera_h264_stream", 10, cameraH264CallBack);
+    dji_osdk_ros::SetupCameraH264 setupCameraH264_;
+    ffmpeg_init();
 
-    bool f = false;
-    bool m = false;
-    char c = 0;
-    std::cout << "Please enter the type of camera stream you want to view (M210 V2)\n"
-              << "m: Main Camera\n"
-              << "f: FPV  Camera" << std::endl;
-    std::cin >> c;
+    char inputChar = 0;
+    std::cout << std::endl;
+    std::cout
+        << "| Available commands:                                          |"
+        << std::endl
+        << "| [a] Start to display FPV stream sample                       |"
+        << std::endl
+        << "| [b] Start to display main camera stream sample               |"
+        << std::endl
+        << "| [c] Start to display vice camera stream sample               |"
+        << std::endl
+        << "| [d] Start to display top camera stream sample                |"
+        << std::endl;
 
-    switch(c)
+    std::cin >> inputChar;
+
+    struct timeval tv;
+    struct tm tm;
+    gettimeofday(&tv, NULL);
+    localtime_r(&tv.tv_sec, &tm);
+
+
+    switch (inputChar) 
     {
-        case 'm':
-        m=true; break;
-        case 'f':
-        f=true; break;
-        default:
-        ROS_DEBUG("No camera selected");
-        return 1;
+      case 'a':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.FPV_CAMERA;
+        setupCameraH264_.request.start        = 1;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
+      case 'b':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.MAIN_CAMERA;
+        setupCameraH264_.request.start        = 1;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
+      case 'c':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.VICE_CAMERA;
+        setupCameraH264_.request.start        = 1;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
+      case 'd':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.TOP_CAMERA;
+        setupCameraH264_.request.start        = 1;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break; 
+      }
     }
 
-    if(f)
-    {
-        setupCameraStream_.request.cameraType = setupCameraStream_.request.FPV_CAM;
-        setupCameraStream_.request.start = 1;
-        setup_camera_stream_client.call(setupCameraStream_);
-    }
-    else if(m)
-    {
-        setupCameraStream_.request.cameraType = setupCameraStream_.request.MAIN_CAM;
-        setupCameraStream_.request.start = 1;
-        setup_camera_stream_client.call(setupCameraStream_);
-    }
-
-    //cameraZoomControl(vehicle);     //run camera zoom control test
-    CameraRGBImage camImg;            //get the camera's image
-
-    // main thread just sleep
-    // callback function will be called whenever a new image is ready
     ros::AsyncSpinner spinner(1);
     spinner.start();
-    ros::Duration(30).sleep();
+    ROS_INFO("Wait 10 second to record stream");
+    ros::Duration(10).sleep();
 
-    if(f)
+    switch (inputChar)
     {
-        setupCameraStream_.request.cameraType = setupCameraStream_.request.FPV_CAM;
-        setupCameraStream_.request.start = 0;
-        setup_camera_stream_client.call(setupCameraStream_);
-    }
-    else if(m)
-    {
-        setupCameraStream_.request.cameraType = setupCameraStream_.request.MAIN_CAM;
-        setupCameraStream_.request.start = 0;
-        setup_camera_stream_client.call(setupCameraStream_);
+      case 'a':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.FPV_CAMERA;
+        setupCameraH264_.request.start        = 0;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
+      case 'b':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.MAIN_CAMERA;
+        setupCameraH264_.request.start        = 0;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
+      case 'c':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.VICE_CAMERA;
+        setupCameraH264_.request.start        = 0;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
+      case 'd':
+      {
+        setupCameraH264_.request.request_view = setupCameraH264_.request.TOP_CAMERA;
+        setupCameraH264_.request.start        = 0;
+        setup_camera_h264_client.call(setupCameraH264_);
+
+        break;
+      }
     }
 
     ROS_INFO_STREAM("Finished. Press CTRL-C to terminate the node");
     ros::waitForShutdown();
+
     return 0;
 }
